14987314b760a773add5d067a9bd405cc67a1c27
PablitoTheChicken <pablitothechicken@outlook.com>
2023-09-01 16:21:50 +0200

    added basic transforms

--- main.cpp
+++ main.cpp
@@ -94,8 +94,14 @@ int main() {
 
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+
     ourShader.use();
     ourShader.setInt("texture1", 0);
+    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
     while(!glfwWindowShouldClose(window)) {
 
